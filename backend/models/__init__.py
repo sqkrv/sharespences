6112dbc51e069280714ccc5692e4f9f794988999
sqkrv <73037424+sqkrv@users.noreply.github.com>
2025-02-12 17:55:40 +0300

    feat(models)!: add `money_type` variable which represents SQLAlchemy's `Numeric(19, 4)` type

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,4 +1,5 @@
 from pydantic.alias_generators import to_camel
+from sqlmodel import Numeric
 
 from backend.db import Base
 
@@ -8,3 +9,6 @@ class CamelBase(Base):
         alias_generator = to_camel
         populate_by_name = True
         from_attributes = True
+
+
+money_type = Numeric(19, 4)
