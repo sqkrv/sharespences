3a4bbc06ff9d533c9398948f548420de1a3d1d13
talebedinec <talebedinec@gmail.com>
2025-03-25 09:30:51 +0300

    added groups and cashbacks, modified icons

---
+++ frontend/src/components/cashback/buttonscashback.tsx
@@ -0,0 +1,19 @@
+export default function ButtonsCashback() {
+    return (
+      <div className="w-full h-[96px] bg-background rounded-xl p-2 flex gap-1 font-sans text-text shadow-custom">
+        <button className="w-full h-full bg-accent rounded-lg flex flex-col justify-end pl-2 pb-2 text-left text-font text-h4 leading-tight whitespace-pre-line">
+          <span>Крутить</span>
+          <span>колесо</span>
+        </button>
+        <button className="w-full h-full bg-accent rounded-lg flex flex-col justify-end pl-2 pb-2 text-left text-font text-h4 leading-tight whitespace-pre-line">
+          <span>Альфа</span>
+          <span>пятница</span>
+        </button>
+        <button className="w-full h-full bg-accent rounded-lg flex flex-col justify-end pl-2 pb-2 text-left text-font text-h4 leading-tight whitespace-pre-line">
+          <span>Ляляля</span>
+          <span>кешбек</span>
+        </button>
+      </div>
+    );
+  }
+  
